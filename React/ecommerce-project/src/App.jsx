23dcc3b6513b698ae19146756b3ab031c64871b3
Cheng Yeh Tsai <181903030+Mason0167@users.noreply.github.com>
2026-05-19 21:19:21 -0400

    Lessong 6: Create more pages(Orders, Tracking etc.). Updated all the links to work with routing. Separated the Header into a component.

--- React/ecommerce-project/src/App.jsx
+++ React/ecommerce-project/src/App.jsx
@@ -1,11 +1,22 @@
-import { HomePage } from './pages/HomePage'
+import { Routes, Route } from 'react-router';
+
+import { HomePage } from './pages/HomePage';
+import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
 
 import './App.css'
 
 function App() {
 
   return (
-    <HomePage />
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
+    </Routes>
+    
   )
 }
 
