686f134d8db8be61efbe744a20633782535f9eb4
Cheng Yeh Tsai <181903030+Mason0167@users.noreply.github.com>
2026-05-21 21:36:29 -0400

    Lesson 7: Separated into smaller components. Fetch data by using async await.

--- React/ecommerce-project/src/App.jsx
+++ React/ecommerce-project/src/App.jsx
@@ -1,36 +1,36 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route } from "react-router";
 
-import { HomePage } from './pages/HomePage';
-import { CheckoutPage } from './pages/checkout/CheckoutPage';
-import { OrdersPage } from './pages/OrdersPage';
-import { TrackingPage } from './pages/TrackingPage';
-import { NotFoundPage } from './pages/NotFoundPage';
+import { HomePage } from "./pages/home/HomePage";
+import { CheckoutPage } from "./pages/checkout/CheckoutPage";
+import { OrdersPage } from "./pages/orders/OrdersPage";
+import { TrackingPage } from "./pages/TrackingPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
-import './App.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/cart-items?expand=product')
-      .then((response) => {
-        console.log(response.data) 
-          setCart(response.data);
-    });
+    const getCartItemData = async () => {
+      const response = await axios.get("/api/cart-items?expand=product")
+      setCart(response.data)
+    };
+
+    getCartItemData();
   }, []);
 
   return (
     <Routes>
       <Route index element={<HomePage cart={cart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
-      <Route path="orders" element={<OrdersPage />} />
+      <Route path="orders" element={<OrdersPage cart={cart} />} />
       <Route path="tracking" element={<TrackingPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
-    
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ React/ecommerce-project/src/pages/home/HomePage.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import { Header } from "../../components/Header";
+import { ProductsGrid } from "./ProductsGrid";
+// import { products } from "../../starting-code/data/products";
+
+import "./HomePage.css";
+
+
+export function HomePage({ cart }) {
+  // fetch('http://localhost:3000/api/products')
+  //     .then((response) => {
+  //         return response.json()
+  //     }).then((data) => {
+
+  //     });
+
+  const [products, setProducts] = useState([]);
+
+  // An easier way to do fetching is axios
+  // Use useEffect so it wont send the request again and again
+  useEffect(() => {
+    const getHomeData = async () => {
+      const response = await axios.get("/api/products")
+      setProducts(response.data);
+    };
+    
+    getHomeData();
+  }, []);
+
+  return (
+    <>
+      <title>Ecommerce Project</title>
+      <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
+
+      <Header cart={cart} />
+
+      <div className="home-page">
+        <ProductsGrid products={products} />
+      </div>
+    </>
+  );
+}
